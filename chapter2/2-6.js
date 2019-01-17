0b097fe448609eb472d54dabc5ca54c3cfda4fe1
Ashkaan Jaberi <ashkaan14@gmail.com>
2019-01-17 17:35:14 +0000

    2-6 done

--- chapter2/2-6.js
+++ chapter2/2-6.js
@@ -4,3 +4,41 @@
 
 const {LinkedList, Node} = require('../lib/linkedlist');
 
+function isPalindrome(ll) {
+  if (!ll.head) return true;
+  
+  let stack = [];
+  let current = ll.head;
+  while (current) {
+    stack.push(current.data);
+    current = current.next;
+  }
+  
+  current = ll.head;
+  
+  while (current) {
+    if (current.data !== stack.pop()) {
+      return false;
+    } else {
+      current = current.next;
+    }
+  }
+  return true;
+}
+
+let ll1 = new LinkedList();
+ll1.insert(1);
+ll1.insert(2);
+ll1.insert(3);
+ll1.insert(4);
+ll1.insert(3);
+ll1.insert(2);
+ll1.insert(1);
+console.log(isPalindrome(ll1) === true);
+
+let ll2 = new LinkedList();
+ll2.insert(5);
+ll2.insert(6);
+ll2.insert(7);
+ll2.insert(6);
+console.log(isPalindrome(ll2) === false)
