9a5f3685048e6f34f5b8b17e49016470240aad77
Ashkaan Jaberi <ashkaan14@gmail.com>
2019-01-28 17:09:02 -0800

    4-1 done

--- chapter4/4-1.js
+++ chapter4/4-1.js
@@ -1,32 +1,65 @@
 class Graph {
-	constructor(){
-		this.nodes = {};
-	}
+  constructor(){
+    this.nodes = {};
+  }
 
-	addNode(value){
-		if(this.nodes[value]) return console.log('value already exists');
-		this.nodes[value] = {};
-	}
+  addNode(value){
+    if(this.nodes[value]) return console.log('value already exists');
+    this.nodes[value] = {};
+  }
 
-	removeNode(node){
-		if(!this.nodes[node]) return console.log('node does not exist');
-		delete this.nodes[node];
-		for(let currNode in this.nodes){
-			if(this.nodes[currNode][node]) delete this.nodes[currNode][node];
-		}
-	}
+  removeNode(node){
+    if(!this.nodes[node]) return console.log('node does not exist');
+    delete this.nodes[node];
+    for(let currNode in this.nodes){
+      if(this.nodes[currNode][node]) delete this.nodes[currNode][node];
+    }
+  }
 
-	addEdge(node, edge) {
-		if(!this.nodes[node]) return console.log('node does not exist');
-		if(this.nodes[node][edge]) return console.log('edge already exsists');
-		this.nodes[node][edge] = true;
-	}
+  addEdge(node, edge) {
+    if(!this.nodes[node]) return console.log('node does not exist');
+    if(this.nodes[node][edge]) return console.log('edge already exsists');
+    this.nodes[node][edge] = true;
+  }
 
-	removeEdge(node,edge) {
-		if(!this.nodes[node]) return console.log('node does not exist');
-		if(!this.nodes[node][edge]) return console.log('edge does not exist');
-		delete this.nodes[node][edge];
-	}
+  removeEdge(node,edge) {
+    if(!this.nodes[node]) return console.log('node does not exist');
+    if(!this.nodes[node][edge]) return console.log('edge does not exist');
+    delete this.nodes[node][edge];
+  }
+	
+  checkRoute(node1, node2) {
+    const q1 = [node1];
+    const visited1 = {[node1]: true};
+
+    const q2 = [node2];
+    const visited2 = {[node2]: true};
+
+    while (q1.length || q2.length) {
+      if (q1.length) {
+        let current = q1.shift();
+        if (current === node2) return true;
+        for (let node in this.nodes[current]) {
+          if (!visited1[node]) {
+            visited1[node] = true;
+            q1.push(node);
+          }
+        }
+      }
+
+      if (q2.length) {
+        let current = q2.shift();
+        if (current === node1) return true;
+        for (let node in this.nodes[current]) {
+          if (!visited2[node]) {
+            visited2[node] = true;
+            q2.push(node);
+          }
+        }
+      }
+    }
+    return false;
+  }
 }
 
 let graph = new Graph();
