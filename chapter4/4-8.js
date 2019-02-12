b98e09d97c27893019e379763de1504aea332307
Ashkaan Jaberi <ashkaan14@gmail.com>
2019-02-11 18:06:36 -0800

    4.8 started

--- chapter4/4-8.js
+++ chapter4/4-8.js
@@ -1,1 +1,14 @@
-https://www.youtube.com/watch?v=13m9ZCB8gjw
+// https://www.youtube.com/watch?v=13m9ZCB8gjw
+
+// LCA binary tree
+function lca(root, node1, node2) {
+  if (!root) return null;
+  if (root === node1 || root === node2) return root;
+  let left = lca(root.left, node1 , node2);
+  let right = lca(root.right, node1, node2);
+  if (left && right) return root;
+  if (!left && !right) return null;
+  return left ? left : right;
+}
+
+
